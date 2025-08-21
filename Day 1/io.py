55de7a009aba5fc4c1e9c1d4ed1964b9fd79f820
anuraggc <anuraggan1501@gmail.com>
2025-08-22 01:09:47 +0530

    Day 1 of python A-Z

---
+++ Day 1/io.py
@@ -0,0 +1,10 @@
+# my_name=input("What is your name?")
+my_age=int(input("How old are you?"))
+
+# print("Hello", my_name)
+print(type(my_age))
+
+
+print("You are", my_age, "years old.")
+print("You are {} years old.".format(my_age))
+print(f"You are {my_age} years old.")
---
+++ Day 1/variables.py
@@ -0,0 +1,7 @@
+my_name="Anurag1"
+
+MY_NAME="Anurag2"
+
+
+print("Hello", my_name)
+print("Hello", MY_NAME)
